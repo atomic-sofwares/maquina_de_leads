f266401c0b516e35c3f0222938ee0a02f6a0aaee
ColdBlak <patrick.ee.doctum@gmail.com>
2018-08-19 15:06:14 -0300

    Alteração sessão

--- acesso/index.php
+++ acesso/index.php
@@ -1,5 +1,5 @@
 <?php
-    require '../sistema/funcoes/sessao.php'
+    //require '../sistema/funcoes/sessao.php'
 ?>
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
@@ -44,7 +44,7 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fa fa-bars"></i>
                 </button>
-                <a class="navbar-brand" href="./"><img src="images/logo.png" alt="Logo"></a>
+                <a class="navbar-brand" href="#">Máquina de Leads</a>
                 <a class="navbar-brand hidden" href="./"><img src="images/logo2.png" alt="Logo"></a>
             </div>
 
@@ -141,13 +141,14 @@
                 <div class="col-sm-7">
                     <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
                     <div class="header-left">
-                        <button class="search-trigger"><i class="fa fa-search"></i></button>
-                        <div class="form-inline">
-                            <form class="search-form">
-                                <input class="form-control mr-sm-2" type="text" placeholder="Search ..." aria-label="Search">
-                                <button class="search-close" type="submit"><i class="fa fa-close"></i></button>
-                            </form>
-                        </div>
+                        <!-- botão pesquisa global -->
+<!--                        <button class="search-trigger"><i class="fa fa-search"></i></button>-->
+<!--                        <div class="form-inline">-->
+<!--                            <form class="search-form">-->
+<!--                                <input class="form-control mr-sm-2" type="text" placeholder="Search ..." aria-label="Search">-->
+<!--                                <button class="search-close" type="submit"><i class="fa fa-close"></i></button>-->
+<!--                            </form>-->
+<!--                        </div>-->
 
                         <div class="dropdown for-notification">
                           <button class="btn btn-secondary dropdown-toggle" type="button" id="notification" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
--- sistema/funcoes/sessao.php
+++ sistema/funcoes/sessao.php
@@ -2,11 +2,11 @@
 session_start();
 if (isset($_SESSION['username']) && $_SESSION['username'] != "") {
     //your contenct here
-    header("Location: /acesso/index.php");
+    header("Location: acesso/index.php");
 
 
 } else {
     //go back if username is not set
-    header("Location: /acesso/page-login.html");
+    header("Location: acesso/page-login.html");
 }
 ?>
