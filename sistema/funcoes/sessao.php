3da47e6b201548734ee7da9f23bcb18e25f53214
marcos <marcos.felipe.gf@gmail.com>
2018-08-19 11:35:12 -0300

    arquivos principais

--- sistema/funcoes/sessao.php
+++ sistema/funcoes/sessao.php
@@ -3,7 +3,7 @@ session_start();
 if (isset($_SESSION['username']) && $_SESSION['username'] != "") {
     //your contenct here
     header("Location: /acesso/index.php");
-    echo $_SESSION['username'] . ' logado';
+
 } else {
     //go back if username is not set
     header("Location: /acesso/page-login.html");
