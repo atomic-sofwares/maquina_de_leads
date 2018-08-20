6124210d6d2647ff81a0e9f79712c16d32dd1e6b
ColdBlak <patrick.ee.doctum@gmail.com>
2018-08-19 22:31:55 -0300

    Criação do cadastro usuário

--- acesso/page-register.php
+++ acesso/page-register.php
@@ -31,7 +31,6 @@
 </head>
 <body class="bg-dark">
 
-
     <div class="sufee-login d-flex align-content-center flex-wrap">
         <div class="container">
             <div class="login-content">
@@ -78,9 +77,15 @@
                             <p>Já possui uma conta? <a href="page-login.php"> Entrar</a></p>
                         </div>
                     </form>
-                <div style="display: none" id="sucesso">
-                        <h3>Usuário cadastrado com sucesso</h3>
-                        <img src="../images/sucesso.png">
+                    <div style="display: none;" id="sucesso">
+                        <img src="../images/sucesso.png" style="width: 30%;left: 35%; position: relative">
+                        <div class="row" style="height: 60%; margin-top: 20px; text-align: center;">
+                            <h3 style="width: 100%">Usuário cadastrado com sucesso!</h3>
+                            <p>Acesse o link de confirmação enviado para seu e-mail, caso não encontre verifique também na caixa de SPAM.</p>
+                            <button type="button" class="btn btn-success" onclick="window.open('page-login.php','_self')">Ir para o login</button>
+                        </div>
+
+
                     </div>
                 </div>
             </div>
