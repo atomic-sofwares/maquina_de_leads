15c83f8498eb9f7a071c8e867eca63febd132078
brunoaalmeida00 <ifesbrunos@gmail.com>
2018-08-25 09:41:47 -0300

    Página de planos adicionada; Logo alterada; Css novo; Bruno

---
+++ acesso/page_plan.php
@@ -0,0 +1,209 @@
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Máquina de Leads</title>
+    <style>
+		.navbar {
+			overflow: hidden;
+			background-color: #333;
+			position: fixed;
+			top: 0;
+			width: 100%;
+			color: #f2f2f2;
+        }
+
+        .navbar a {
+            float: left;
+            display: block;
+			text-align: center;
+			padding: 14px 16px;
+			text-decoration: none;
+			font-size: 17px;
+		}
+
+		.navbar a:hover {
+			background:black;
+			color: #ddd;
+		}
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: white;
+            font-family: Segoe UI;
+			font-weight: bold;
+        }
+
+       	 li a {
+            display: block;
+            color: black;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+			font-family: Segoe UI;
+
+        }
+
+        li a:hover {
+            background-color: white;
+		}
+    </style>
+
+	<link rel="shortcut icon" href="favicon.png">
+	<!-- Custom Theme files -->
+	<link href="../acesso/assets/css/plan_style.css" rel="stylesheet" type="text/css" media="all"/>
+	<!-- for-mobile-apps -->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Colorful Pricing Tables Widget Responsive, Login form web template, Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<!-- //for-mobile-apps -->
+	<!--Google Fonts-->
+	<link href='//fonts.googleapis.com/css?family=Monda:400,700' rel='stylesheet' type='text/css'>
+	<!--google fonts-->
+	<script src="../acesso/assets/js/jquery-1.11.0.min.js"></script>
+	<script src="../acesso/assets/js/jquery.magnific-popup.js" type="text/javascript"></script><script>
+			$(document).ready(function() {
+			$('.popup-with-zoom-anim').magnificPopup({
+				type: 'inline',
+				fixedContentPos: false,
+				fixedBgPos: true,
+				overflowY: 'auto',
+				closeBtnInside: true,
+				preloader: false,
+				midClick: true,
+				removalDelay: 300,
+				mainClass: 'my-mfp-zoom-in'
+			});
+
+			});
+	</script>
+
+</head>
+<body style=" align-content: center">
+<!--header start here-->
+<ul class="navbar">
+    <li style="float: left"><a class="active" href="#home">Home</a></li>
+    <li style="float: left"><a href="#news">News</a></li>
+    <li style="float: left"><a href="#contact">Contact</a></li>
+    <li style="float: left"><a href="#about">About</a></li>
+</ul>
+
+
+
+<div class="priceing-table w3l" >
+	<div class="wrap">
+		<div class="priceing-table-main" style="position:relative; left:  -0.5%; top: 50px;">
+			<img class="" src="images/logo_1.png" style="position: relative; left: 33%;" width="33%"  alt="Logo">
+		</div>
+			<div class="priceing-table-main" style="margin-top: 50px;">
+			<div class="price-grid">
+	    		   <div class="price-block agile" >
+		    			<div class="price-gd-top pric-clr1">
+		    				<h4 style="font-family: Segoe UI">Básico</h4>
+		    				<h3>R$5,00</h3>
+		    				<h5>1 mês</h5>
+		    			</div>
+		    			<div class="price-gd-bottom">
+		    				<div class="price-list">
+			    				<ul>
+			    					<li>Full access</li>
+			    					<li>Documentation</li>
+			    					<li>Customers Support</li>
+			    				</ul>
+		    				</div>
+		    			</div>
+		    		    <div class="price-selet pric-sclr1">		    			   
+		    			   	  <a class="popup-with-zoom-anim" href="#small-dialog">Assinar</a>
+		    			</div>
+		    		</div>
+	    		</div>
+	    		<div class="price-grid">
+	    			<div class="price-block agile"  >
+		    			<div class="price-gd-top pric-clr2">
+		    				<h4 style="font-family: Segoe UI">Intermediário</h4>
+		    				<h3>R$10,00</h3>
+		    				<h5>5 meses</h5>
+		    			</div>
+		    			<div class="price-gd-bottom">
+		    				<div class="price-list">
+			    				<ul>
+			    					<li style="text-align: center">Full access</li>
+			    					<li style="text-align: center">Documentation</li>
+			    					<li style="text-align: center">Customers Support</li>
+			    				</ul>
+		    				</div>
+		    			</div>
+		    		    <div class="price-selet pric-sclr2">
+		    			   	 <a class="popup-with-zoom-anim" href="#small-dialog">Assinar</a>
+		    			</div>
+		    		</div>
+	    		</div>
+	    		<div class="price-grid wthree"  >
+	    			<div class="price-block agile">
+		    			<div class="price-gd-top pric-clr3">
+		    				<h4 style="font-family: Segoe UI">Avançado</h4>
+		    				<h3>R$20,00</h3>
+		    				<h5>Gratuito por 2 meses</h5>
+		    			</div>
+		    			<div class="price-gd-bottom">
+		    				<div class="price-list">
+			    				<ul>
+			    					<li>Full access</li>
+			    					<li>Documentation</li>
+			    					<li>Customers Support</li>
+			    				</ul>
+		    				</div>
+		    			</div>
+		    		    <div class="price-selet pric-sclr3">
+		    			   	<a class="popup-with-zoom-anim" href="#small-dialog">Assinar</a>
+		    			</div>
+		    		</div>
+    	       </div>
+             <div class="clear"> </div>
+		</div>
+	</div>
+</div>
+<!--header end here-->
+<!--pop-up-grid-->
+<div id="popup">
+<div id="small-dialog" class="mfp-hide">
+<div class="pop_up">
+	<div class="payment-online-form-left">
+		<form action="#" method="post">
+			<h4>Cadastre-se</h4>
+			<ul>
+				<li><input class="text-box-dark" type="text" placeholder="Nome" name="Name" required=""></li>
+				<li><input class="text-box-dark" type="text" placeholder="E-mail" name="Email" required=""></li>
+				<li><input class="text-box-dark" type="password" placeholder="Senha" name="Password" required=""></li>
+				<li><input class="text-box-dark" type="text" placeholder="Telefone" name="Phone" required=""></li>
+			</ul>																				
+			<span class="checkbox1">
+					<label class="checkbox"><input type="checkbox" name="" checked=""><i> </i>Eu aceito os termos.</label>
+				</span>												
+			<ul class="payment-sendbtns">
+				<li><input type="submit" value="Cadastrar"></li>
+			</ul>
+		</form>
+	</div>
+</div>
+</div>
+</div>
+								
+<!--pop-up-grid-->
+
+<!--copy rights end here-->
+<!--<div class="copy-rights">
+	<p>© 2016 Colorful Pricing Tables. All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>		 	
+</div>-->
+<!--copyrights start here-->
+
+</body>
+</html>
