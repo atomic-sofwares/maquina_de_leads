e19d144e98f7c4c63cbaafa604d00c0e8a48e608
marcos <marcos.felipe.gf@gmail.com>
2018-08-26 17:14:05 -0300

    blocos do dash

--- acesso/index.php
+++ acesso/index.php
@@ -81,82 +81,9 @@ require '../sistema/funcoes/sessao.php'
 
 
         <?php require_once 'blocos_dash/bloco_visitas_page_hoje.php' ?>
-        <?php require_once 'blocos_dash/bloco_visitas_page_hoje.php' ?>
-        <?php require_once 'blocos_dash/bloco_visitas_page_hoje.php' ?>
-        <?php require_once 'blocos_dash/bloco_visitas_page_hoje.php' ?>
-
-
-
-        <div class="col-lg-3 col-md-6">
-            <div class="social-box facebook">
-                <i class="fa fa-facebook"></i>
-                <ul>
-                    <li>
-                        <strong><span class="count">40</span> k</strong>
-                        <span>friends</span>
-                    </li>
-                    <li>
-                        <strong><span class="count">450</span></strong>
-                        <span>feeds</span>
-                    </li>
-                </ul>
-            </div>
-            <!--/social-box-->
-        </div><!--/.col-->
-
-
-        <div class="col-lg-3 col-md-6">
-            <div class="social-box twitter">
-                <i class="fa fa-twitter"></i>
-                <ul>
-                    <li>
-                        <strong><span class="count">30</span> k</strong>
-                        <span>friends</span>
-                    </li>
-                    <li>
-                        <strong><span class="count">450</span></strong>
-                        <span>tweets</span>
-                    </li>
-                </ul>
-            </div>
-            <!--/social-box-->
-        </div><!--/.col-->
-
-
-        <div class="col-lg-3 col-md-6">
-            <div class="social-box linkedin">
-                <i class="fa fa-linkedin"></i>
-                <ul>
-                    <li>
-                        <strong><span class="count">40</span> +</strong>
-                        <span>contacts</span>
-                    </li>
-                    <li>
-                        <strong><span class="count">250</span></strong>
-                        <span>feeds</span>
-                    </li>
-                </ul>
-            </div>
-            <!--/social-box-->
-        </div><!--/.col-->
-
-
-        <div class="col-lg-3 col-md-6">
-            <div class="social-box google-plus">
-                <i class="fa fa-google-plus"></i>
-                <ul>
-                    <li>
-                        <strong><span class="count">94</span> k</strong>
-                        <span>followers</span>
-                    </li>
-                    <li>
-                        <strong><span class="count">92</span></strong>
-                        <span>circles</span>
-                    </li>
-                </ul>
-            </div>
-            <!--/social-box-->
-        </div><!--/.col-->
+        <?php require_once 'blocos_dash/bloco_visitas_totais_page.php' ?>
+        <?php require_once 'blocos_dash/bloco_cadastros_hoje_page.php' ?>
+        <?php require_once 'blocos_dash/bloco_cadastros_totais_page.php' ?>
 
         <div class="col-xl-6">
             <div class="card">
